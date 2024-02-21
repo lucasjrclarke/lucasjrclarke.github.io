1da4ce2057e9cf454f8ac4fc3525fe215886d5f2
lucasjrclarke <lukeyspud@gmail.com>
2024-02-22 09:39:08 +1100

    Moved portfolio into repo
    Created on local save, and now copied over to repo before further work is done.

---
+++ Data/inter.js
@@ -0,0 +1,28 @@
+/* note: 99 is the code for a featured inter */
+const intersData = {
+    "success": true,
+    "data": {
+        "inters": [
+            {
+                "id": 1,
+                "categoryIds": [1, 99],
+                "shortTitle": "Ohonua",
+                "longTitle": "The 'Ohonua' redesign",
+                "shortDescription": "A urban planning project with a real life case study",
+                "description": "In my second year of university, I took on Urban Planning as a new field of study, and was presented with this end of semester project. The island of Eua, in Tonga, was devastated by natural disasters in the year before, and as part of a competition our group created a sustainable solution for the rebuilding of Ohonua, a town on the island. This project was subsequently created to have a real impact on the world, and an incredible experience to be a part of.",
+                "siteLink": "",
+                "navLink": "",
+                "coverImage": "",
+                "heroImage": "",
+                "gallery": [],
+                "galleryDescription": "",
+                "knowledge": ["A skill", "A skill"],
+                "story": "",
+                "storyImages": [],
+                "software": [8, 9, 10, 13, 14, 16]
+            },
+        ]
+    }
+};
+
+export default intersData;
